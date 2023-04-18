f20a8291a617a6c6ae828b5e90535ee45c146324
JehanIbrahem <jibrahem@ucsb.edu>
2023-04-18 13:12:14 -0700

    added all database

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Spot.belongsTo(models.User, {foreignKey: 'ownerId'});
+      Spot.hasMany(models.SpotImage, {foreignKey: 'spotId'});
+      Spot.hasMany(models.Booking, {foreignKey: 'userId'});
+      Spot.hasMany(models.Review, {foreignKey: 'spotId'});
     }
   }
   Spot.init({
