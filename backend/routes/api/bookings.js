468e1a5c072383a109a09ebe55f54eee41dee6ee
JehanIbrahem <jibrahem@ucsb.edu>
2023-04-21 15:43:21 -0700

    almost done w booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -19,13 +19,16 @@ router.get('/current', requireAuth, async (req, res) =>{
 router.put('/:bookingId', requireAuth, async (req, res) => {
     const booking = await Booking.findByPk(req.params.bookingId);
     const {startDate, endDate} = req.body;
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
     if(!booking){
-        res.status(404).json({
+        return res.status(404).json({
             message: "Booking couldn't be found"
         });
     }
-    if(endDate <= startDate){
-        res.status(400).json({
+    if(end < start){
+        return res.status(400).json({
             endDate: "endDate cannot come before startDate"
         });
     }
@@ -35,13 +38,37 @@ router.put('/:bookingId', requireAuth, async (req, res) => {
             message: "Past bookings can't be modified"
         });
     }
-    if(startDate){
-        booking.startDate = startDate;
+    if(start){
+        booking.startDate = start;
     }
-    if(endDate){
-        booking.endDate = endDate;
+    if(end){
+        booking.endDate = end;
     }
-    
+    return res.json(booking);
+});
+
+router.delete('/:bookingId', requireAuth, async (req, res)=>{
+    const {user} = req;
+    const booking = await Booking.findByPk(req.params.bookingId, {
+        where: {userId : user.id}
+});
+    if(!booking){
+        return res.status(404).json({
+            message: "Booking couldn't be found"
+        });
+    }
+    const now = new Date();
+    if(booking.startDate > now){
+       return res.status(403).json({
+            message: "Bookings that have been started can't be deleted"
+        });
+    }
+
+    await booking.destroy();
+    return res.json({
+        message: "Successfully deleted"
+    });
+
 });
 
 
